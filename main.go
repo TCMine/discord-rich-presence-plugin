349571994f48bd0f6bc93319ae785539db93b109
TCMine <32180878+TCMine@users.noreply.github.com>
2026-08-22 14:33:53 -0500

    dc

--- main.go
+++ main.go
@@ -218,7 +218,7 @@ func (p *discordPlugin) handlePlayingOrPaused(input scrobbler.PlaybackReportRequ
 			assets.SmallImage = pauseIconURL
 			assets.SmallText = "Paused"
 		} else {
-			rpc.clearActivity(input.Username)
+			p.handleStopped(input)
 		}
 		
 	}
